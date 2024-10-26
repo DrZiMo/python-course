d4c587c3bd1992f805d73f10cf2e09d09086aacc
drzimo <zuhaybhamar@gmail.com>
2024-10-26 19:51:45 +0300

    Collatz Challenge added

--- pythonLessons.py
+++ pythonLessons.py
@@ -10,3 +10,28 @@ for price in range(10):
 
 print("I have", wallet, "dollars")
 print("and", socks, "socks")
+
+time = 3
+
+for n in range(3):
+    if n < time:
+        print(n)
+    elif n == time:
+        print("go")
+
+number = 27
+steps = 0
+
+for i in range(200):
+    if number == 1:
+        break
+    elif number % 2 == 0:
+        number = number / 2
+    else:
+        number = number * 3 + 1
+    steps = steps + 1
+
+if number == 1:
+    print("it took", steps, "steps")
+else:
+    print("number didn't reach 1 yet")
