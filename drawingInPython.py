732b8943715a52e7c714e98343aeff5c0501af31
drzimo <zuhaybhamar@gmail.com>
2024-10-26 20:08:27 +0300

    drawing in python using turtle

---
+++ drawingInPython.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+#Squrare
+color('red')
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+#triangle
+color('blue')
+right(90)
+forward(200)
+right(135)
+forward(285)
+
+bye()
