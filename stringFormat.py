06d130c844ded6cae66a32ea5771e2aa504b4e09
drzimo <zuhaybhamar@gmail.com>
2024-10-29 21:15:51 +0300

    replacement function

--- stringFormat.py
+++ stringFormat.py
@@ -8,3 +8,12 @@ print(word[1:5])  # earn
 
 word2 = "embossed"
 print(word2[2:6])  # boss
+
+# replacing function
+aboutWeather = "It will be sunny today."
+print(aboutWeather.replace("sunny", "rainy"))
+
+email = "Hello, [insert name here]. i'd like to personally thank you for your contribution"
+email = email.replace("[insert name here]", "Zuhayb")
+
+print(email)
