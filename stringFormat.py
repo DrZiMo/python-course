b8fe46a1802186d5921155d538515d22f19e38da
drzimo <zuhaybhamar@gmail.com>
2024-10-29 21:09:46 +0300

    sting subset

--- stringFormat.py
+++ stringFormat.py
@@ -1,3 +1,10 @@
 str1 = "Hello"
-str1Index = str1.find("llo")  #get the index of the l
+str1Index = str1.find("llo")  # get the index of the l
 print(str1[str1Index])
+
+# substring
+word = "learning"
+print(word[1:5])  # earn
+
+word2 = "embossed"
+print(word2[2:6])  # boss
