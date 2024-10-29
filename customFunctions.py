dbc2e007964f46c687e8ee8047535e076f46c6c2
drzimo <zuhaybhamar@gmail.com>
2024-10-29 21:33:13 +0300

    intresting circle

--- customFunctions.py
+++ customFunctions.py
@@ -6,15 +6,19 @@ from turtle import *
 #     left(9)
 
 # first custom function
-def left_turn():
-    color("red", "yellow")
+def left_turn(number_of_turns, fill_color):
+    color("red", fill_color)
     begin_fill()
-    for i in range(40):
+    for i in range(number_of_turns):
         forward(15)
         left(9)
     end_fill()
 
 
-left_turn()
+left_turn(40, "yellow")
+left_turn(10, "blue")
+left_turn(10, "pink")
+left_turn(10, "purple")
+left_turn(10, "green")
 
 bye()
